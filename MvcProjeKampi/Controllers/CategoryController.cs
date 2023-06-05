057a33fbd26944ac6911054de21dc925a24bf630
Erdem Taş <erdemtas60@gmail.com>
2023-06-05 16:01:29 +0300

    Add project files.

---
+++ MvcProjeKampi/Controllers/CategoryController.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class CategoryController : Controller
+    {
+		CategoryManager cm = new CategoryManager();
+
+		// GET: Category
+		public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetCategoryList()
+        {
+            var categoryValues = cm.GetAllBL();
+            return View(categoryValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory() { 
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            cm.CategoryAddBL(p);
+            return RedirectToAction("GetCategoryList");
+        }
+    }
+}
